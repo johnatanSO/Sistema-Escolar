10c17ad37b3b22df8694972758439f58684853b6
Johnatan Santos <jonystrit@gmail.com>
2022-01-13 15:26:32 -0300

    update modelContent

--- pages/teacher/script.js
+++ pages/teacher/script.js
@@ -1,3 +1,25 @@
 function logOut(){
     window.location.href = "../../index.html"
 }
+
+let closeButton = document.querySelector("#fechar")
+
+closeButton.addEventListener("click", ()=>{
+    let modalNotas = document.getElementsByClassName("modalNotas")[0]
+    modalNotas.style.display = "none"
+})
+
+
+
+
+let buttonNotas = document.querySelector(".notasContent")
+buttonNotas.addEventListener("click", ()=>{
+    let modalNotas = document.querySelector(".modalNotas")
+    modalNotas.style.display = "flex"
+})
+
+let buttonAdvertencia = document.querySelector(".advertenceContent")
+buttonAdvertencia.addEventListener("click", ()=>{
+    let modalAdvertencia = document.querySelector(".modalAdvertencia")
+    modalAdvertencia.style.display = "flex"
+})
