695a43036fddb88774da6b6f1022b36e9f359628
Johnatan Santos <jonystrit@gmail.com>
2022-01-13 16:42:30 -0300

    Create notes table

--- pages/teacher/script.js
+++ pages/teacher/script.js
@@ -2,24 +2,27 @@ function logOut(){
     window.location.href = "../../index.html"
 }
 
-let closeButton = document.querySelector("#fechar")
-
-closeButton.addEventListener("click", ()=>{
-    let modalNotas = document.getElementsByClassName("modalNotas")[0]
-    modalNotas.style.display = "none"
-})
-
-
-
-
 let buttonNotas = document.querySelector(".notasContent")
 buttonNotas.addEventListener("click", ()=>{
     let modalNotas = document.querySelector(".modalNotas")
     modalNotas.style.display = "flex"
+
+    let closeButton = document.querySelectorAll(".fechar")[0]
+    closeButton.addEventListener("click", ()=>{
+        let modalNotas = document.querySelector(".modalNotas")
+        modalNotas.style.display = "none"
+
+    })
 })
 
 let buttonAdvertencia = document.querySelector(".advertenceContent")
 buttonAdvertencia.addEventListener("click", ()=>{
     let modalAdvertencia = document.querySelector(".modalAdvertencia")
     modalAdvertencia.style.display = "flex"
+
+    let closeButton = document.querySelectorAll('.fechar')[1]
+    closeButton.addEventListener("click", ()=>{
+        let modalAdvertencia = document.querySelector(".modalAdvertencia")
+        modalAdvertencia.style.display = "none"
+    })
 })
