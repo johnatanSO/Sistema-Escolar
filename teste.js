ce0e6fd13aa246c3edd4c28d57c7df5db9ae0731
Johnatan Santos <devsantosjohn@gmail.com>
2022-02-10 11:57:58 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,8 +0,0 @@
-  /* --------------------------------------------- */
-console.log()
-  db.collection('teste').get()
-    .then((snapshot)=>{
-        snapshot.forEach((element)=>{
-            console.log(element.data())
-        }) 
-    })
